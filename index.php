ad6c61fb723031a4f37e23a8f350721a2745eded
Giustino Borzacchiello <giustinob@gmail.com>
2014-09-07 20:54:38 +0200

    Added select with positional placeholder

--- index.php
+++ index.php
@@ -7,4 +7,13 @@ $connectionParams = require_once __DIR__ . '/connection_params.php';
 $config = new \Doctrine\DBAL\Configuration();
 $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
-echo "Connection OK";
+// simple query
+$sql = "SELECT * FROM actor";
+$stmt = $conn->query($sql);
+
+// query with positional placeholder
+$sql = "SELECT * FROM actor WHERE actor_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(1, 10);
+$stmt->execute();
+
